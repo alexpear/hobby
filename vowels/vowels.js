e815f80a8ff3ef48e63dd495424176b96bfb9b2d
Alex Pear <alexmpear@gmail.com>
2016-02-15 01:18:37 -0500

    Remove some debug code

--- vowels/vowels.js
+++ vowels/vowels.js
@@ -91,35 +91,15 @@ function isCoreVowel(char) {
 };
 
 
-
 // run
 var wordsPath = '/usr/share/dict/words';
 fs.readFile(wordsPath, 'utf8', function (err, words) {
   if (err) { return console.log(err); }
 
-  console.log('before withEnoughVowels');
   var varietousWords = withEnoughVowels(words.split('\n'), 4);
-  console.log('varietousWords length is ' + varietousWords.length);
 
   varietousWords.forEach(function(word) {
     console.log(word);
-    // console.log(vowelTypesIn(word));
   });
 });
 
-console.log('after readfile');
-
-
-
-// tests
-// console.log(isCoreVowel('E'));
-// console.log(isCoreVowel('Q'));
-// console.log(isCoreConsonant('E'));
-// console.log(isCoreConsonant('Q'));
-
-// console.log(containsYVowel('EASY'));
-// console.log(containsYVowel('YOUNG'));
-
-// console.log(vowelTypesIn('YOUNG'));
-// console.log(vowelVariety('YOUNG'));
-// console.log(withEnoughVowels(['YOUNG', 'EASY'], 3));
