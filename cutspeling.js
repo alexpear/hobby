5c66b9fa56e6c967ede9ba481bd41c3d636fd639
Alex Pear <alexmpear@gmail.com>
2016-04-08 18:12:01 -0400

    Add incomplete skeleton for cut spelling tools

---
+++ cutspeling.js
@@ -0,0 +1,14 @@
+// cut speling
+
+var cutMap = {
+	the: 'th',
+	'don\'t': 'dont',
+	inexpressible: 'inexpresibl'
+};
+
+// function wishlist
+// remove silent trailing Es
+// remove double letters
+// reduce dipthongs, dipthong map
+// GH
+// silent H
