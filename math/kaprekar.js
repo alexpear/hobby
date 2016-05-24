4042c012f6481574c2d57a598e196e3cf7f871e6
Alex Pear <alexmpear@gmail.com>
2016-05-24 13:12:58 -0400

    Add script for finding cycles of Kaprekar constants
    * eg 6174

---
+++ math/kaprekar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var kaprekar = module.exports;
+
+var leftPad = require('left-pad');
+
+kaprekar.next = function (origString) {
+	var nextInt =
+		parseInt(origString.split('').sort().reverse().join('')) -
+		parseInt(origString.split('').sort().join(''));
+	return leftPad(nextInt, origString.length, 0);
+};
+
+function run () {
+	// For when this file is run as a command line script:
+	if (process.argv.length !== 3) {
+		console.log('usage: node kaprekar.js <integer>');
+	} else {
+		var curNum = process.argv[2];
+		console.log(curNum);
+		var successors = {};
+		while (! successors[curNum]) {
+			successors[curNum] = kaprekar.next(curNum);
+			console.log(successors[curNum]);
+			curNum = successors[curNum];
+		}
+	}
+}
+
+// todo: make compatible with module mode
+run();
