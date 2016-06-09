7dece1b93e9e9abb503232efca7194bdf7791e90
Alex Pear <alexmpear@gmail.com>
2016-06-09 16:15:43 -0400

    Flesh out lineAsArray()

--- p2p/csv.js
+++ p2p/csv.js
@@ -1,17 +1,20 @@
 // This will later become a module.
 
-var defaultSeparator = '|';
+var DEFAULT_SEPARATOR = '|';
 
 var exampleHeader = 'magnet|ipfs|seeders|date|size|name';
 var exampleLine = '19438GHQE134GHF8O7QASDHFHQ3V9WADFB98SH|QmU5XsVwvJfTcCwqkK1SmTqDmXWSQWaTa7ZcVLY2PDxNxG|68|2016-06-30 1612|202M|Works of William Shakespeare';
 
 function lineAsArray (lineString, separator) {
-    separator = separator || defaultSeparator;
+    separator = separator || DEFAULT_SEPARATOR;
+    var BACKSLASH = '\\';
 
-    if (lineString contains escaped '\|') {
-
+    if (lineString.indexOf(BACKSLASH + separator) > -1) {
+        // TODO: Better logging and error handling
+        console.log('ERROR: sorry, lineAsArray() doesnt support escaped ' + separator + ' characters inside cells.');
+        return;
     } else {
-        return lineString.split(separator)
+        return lineString.split(separator);
     }
 }
 
