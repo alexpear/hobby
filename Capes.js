81ab6917b03f3c286041722e0aa4d0eb248724c8
Alex Pear <alexmpear@gmail.com>
2016-06-22 18:41:01 -0400

    Use object as constructor param

--- Capes.js
+++ Capes.js
@@ -35,23 +35,53 @@ var Cape = class Cape {
 module.exports.Cape = Cape;
 
 var profiles = module.exports.profiles = {};
-profiles.HER_CAPTAIN_AMERICA = new Cape(
-    'Captain America',
-    {
+profiles.HER_CAPTAIN_AMERICA = new Cape({
+    name: 'Captain America',
+    ratings: {
         brute: 4,
         blaster: 3,
         mover: 2,
         striker: 1
     },
-    Allegiance.HERO,
-    [
+    allegiance: Allegiance.HERO,
+    notes: [
         'Handheld vibranium shield',
         'Uses shield as a thrown weapon. Capable of ricocheting into multiple targets.',
         'Uses small arms weapons',
         'Uses spycraft',
         'Close combat training',
         'Born in 1919, cryogenically preserved 1945-2011',
-        'World\'s first superhero.'
+        'World\'s first superhero'
+    ]
+});
+profiles.HER_HULK = new Cape({
+    name: 'The Incredible Hulk',
+    ratings: {
+        changer: 5,
+        brute: 10,
+        mover: 4
+    },
+    allegiance: Allegiance.HERO,
+    notes: [
+        'Unchanged form is unpowered but retains invulnerability',
+        'Transforms in elevated emotional states',
+        'Unpredictable, "berserker" combat style',
+        'Powers come from gamma radiation experiment',
+        'Mover 4: Jump height of approximately 10 storeys'
+    ]
+});
+profiles.HER_HAWKEYE = new Cape({
+    name: 'Hawkeye',
+    ratings: {
+        tinker: 4,
+        blaster: 6,
+        mover: 3
+    },
+    allegiance: Allegiance.HERO,
+    notes: [
+        'No powers beyond Tinker',
+        'Arrow types include: explosive, harpoon, electric',
+        'SHIELD agent'
     ]
 });
 
