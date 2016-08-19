6e8eb40fbfe5f56702830bd62167e9427259f775
Alex Pear <alexmpear@gmail.com>
2016-08-18 21:25:14 -0400

    Add skeleton of IF scifi complexgen

---
+++ genregen/BeveledPlains.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// BevelledPlains.js
+// There is a planet far from here, woven from earth and steel in strange patterns. 
+// Over the empty surface the wind winds peacefully between the trees
+// and the standby lights blink.
+
+
+
+var COLORS = [
+    'blue',
+    'green',
+    'red',
+    'white',
+    'grey',
+    'yellow',
+    'orange',
+    'gold',
+    'magenta',
+    'aquamarine',
+    'violet',
+    'brown',
+    'sage'
+];
+
+function exampleComplex () {
+
+}
+
+// Incomplete
+function Site (extantChannels) {
+    return {
+        description: 'todo',
+        neighbors: [
+
+        ]
+    };
+}
+
