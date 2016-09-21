835cbbd5e98d082fc8e701cfadadd64daa0006d4
Alex Pear <alexmpear@gmail.com>
2016-09-21 17:11:25 -0400

    Add skeleton of library for solving 3d pentamino puzzles

---
+++ math/blocks.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Cube-based 3d puzzles
+
+var _ = require('underscore');
+
+class Quaternion {
+    constructor (x, y, z, rotation) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rotation = rotation;  // Name may change.
+    }
+
+    add (otherQuat) {
+        // TODO
+    }
+}
+
+class Piece {
+    constructor (cubes, quaternion, id) {
+        this.cubes = cubes;
+        this.quaternion = quaternion;
+        this.id = id;
+    }
+
+    rotate (otherQuat) {
+        this.quaternion.add(otherQuat);
+    }
+
+    getPositions () {
+        // TODO: Return each coord transformed by the quaternion
+        return this.cubes;
+    }
+
+    boundingBox () {
+
+    }
+}
+
+// A set of rotated and positioned Pieces
+class Arrangement {
+    constructor (pieces) {
+        this.pieces = pieces;
+    }
+
+    ids () {
+        return _.pluck(this.pieces, 'id');
+    }
+
+    boundingBox () {
+
+    }
+
+    add (newPiece) {
+        // TODO: check for id / duplicates perhaps.
+        this.pieces.push(newPiece);
+    }
+
+    remove (id) {
+
+    }
+
+    render () {
+        this.renderAsLayers();
+    }
+
+    renderAsLayers () {
+        // TODO: take quaternion into account.
+        // Get bounding box
+        // Set up ascii grid buffers...
+        // or something.
+    }
+
+    collisions () {
+        // TODO
+        return [];
+    }
+
+    valid () {
+        return this.collisions().length === 0;
+    }
+}
