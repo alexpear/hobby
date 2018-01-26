9bae643b33755523de5f340be9e229315cd37e3b
Alex Pear <alex@havenlife.com>
2018-01-25 23:32:54 -0500

    Expand color code in Util

--- warband/util.js
+++ warband/util.js
@@ -26,21 +26,44 @@ Util.stringify = function (x) {
     );
 };
 
+Util.COLORS = {
+    red: '\x1b[1;37;41m',
+    yellow: '\x1b[1;37;43m',
+    green: '\x1b[1;30;42m',
+    cyan: '\x1b[1;30;46m',
+    blue: '\x1b[1;37;44m',
+    purple: '\x1b[1;37;45m',
+    grey: '\x1b[1;30;47m',
+    black: '\x1b[1;37;40m'
+};
+
 Util.colored = function (str, color) {
-    const BALANCE = '\e[0m';
-    const COLORFORM = '\e[34m';
+    const BALANCE = '\x1b[0m';
+    const colorStart = Util.COLORS[color] || Util.COLORS.purple;
+    return colorStart + str + BALANCE;
 };
 
 // Util.sum = function (array) {}
 
 Util.log = function (input, tag) {
+    const TAG_COLORS = {
+        error: 'red',
+        beacon: 'yellow'
+    };
+
+    const tagColor = TAG_COLORS[tag.toLowerCase()];
+    const tagStr = tagColor ?
+        Util.colored(tag.toUpperCase(), tagColor) :
+        tag;
+
     const dateTime = moment().format('YYYY MMM D hh:mm:ss.S');
+
     const info = Util.isString(input) ?
         input :
         Util.stringify(input);
 
     // Later: Red error and beacon text
-    console.log(`  ${tag} (${ dateTime }) ${ info }\n`);
+    console.log(`  ${tagStr} (${ dateTime }) ${ info }\n`);
 };
 
 Util.logError = function (input) {
