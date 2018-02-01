24589fe0518bc8928f904ffb3d7b77fcff0acdf7
Alex Pear <alex@havenlife.com>
2018-02-01 15:40:50 -0500

    Expand GameState slightly

--- warband/gameState.js
+++ warband/gameState.js
@@ -1,10 +1,24 @@
 'use strict';
 
+const chalk = require('chalk');
 
+const Coord = require('./coord.js');
+const Faction = require('./faction.js');
+const Squad = require('./squad.js');
+const Util = require('./util.js');
 
-class GameState {
+module.exports = class GameState {
     constructor () {
+        this.factions = [];
+    }
 
+    getFaction (squad) {
+        // Later might make use of a id or circular reference.
+        return this.factions.find(
+            faction => faction.squads.find(
+                candidateSquad => candidateSquad === squad
+            )
+        );
     }
 
     los (a, b) {
