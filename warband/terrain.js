4dd2bcd5d672d0d1157fd04c91cdaa5f16c64c2b
Alex Pear <alex@havenlife.com>
2018-02-01 17:18:01 -0500

    Expand Terrain slightly

--- warband/terrain.js
+++ warband/terrain.js
@@ -1,9 +1,19 @@
 'use strict';
 
+// - Later figure out the rules for soft cover (occludes but does not block shots).
+// - For now just consider it weaker hard cover
 
-
-class Terrain {
+module.exports = class Terrain {
     constructor () {
 
     }
-}
+
+
+};
+
+Terrain.exampleTerrain = function () {
+    return {
+        name: 'Sparse forest',
+        cover: 0.1
+    };
+};
