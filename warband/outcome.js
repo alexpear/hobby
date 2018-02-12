e7494a0aa21695f49aec4929efca7c46a78ccaf8
Alex Pear <alex@havenlife.com>
2018-02-11 22:04:11 -0800

    Add Outcome to represent stochastic event results

---
+++ warband/outcome.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// A Replay stores Outcomes to track what happened
+// during stochastic events.
+
+let Outcome = module.exports = class Outcome {
+    constructor (shot, victim, attacker) {
+        // Later there will be more types.
+        this.type = 'injury';
+        // Later deal with issues of circular reference.
+        this.shot = shot;
+        this.attacker = attacker && attacker.id;
+        this.victim = victim.id;
+    }
+
+    static exampleOutcome () {
+        return {
+            type: 'injury',
+            shotTemplate: 'Rifle Bullet',
+            attacker: new WNode('marine').id,
+            victim: new WNode('marine').id
+        };
+    }
+};
