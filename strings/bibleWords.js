6ba0a229f75c02a4948a558601005c52c8433173
alexpear <alex@havenlife.com>
2020-06-07 00:15:07 -0400

    Add public domain texts

---
+++ strings/bibleWords.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// For pictionary vocab
+
+const fs = require('fs');
+
+class BibleWords {
+    static run (filename) {
+        // console.log(`top of run()`)
+        BibleWords.makeDictionaries(filename);
+    }
+
+    static makeDictionaries (filename) {
+        const bibleString = fs.readFileSync(filename, { encoding: 'utf8' });
+
+        const alphanumericBible = BibleWords.alphanumericOnly(bibleString);
+        const wordArray = alphanumericBible.split(/\s/);
+
+        const commonWords = {}; // TODO
+
+        const dictionary = {};
+        const capitalizedDictionary = {};
+        
+        // console.log(`start of loop`)
+        
+        for (let word of wordArray) {
+            // console.log(`word is ${word}`)
+
+            if (
+                ! word ||
+                commonWords[word.toLowerCase()] ||
+                BibleWords.containsNumbers(word)
+            ) {
+                continue;
+            }
+
+            if (/[A-Z]/.test(word[0])) {
+                capitalizedDictionary[word] = true;
+            }
+            else {
+                dictionary[word] = true;
+            }
+
+            // console.log(`reached bottom of loop for word "${word}"`)
+        }
+
+        // console.log(`end of loop`)
+
+        const capitalizedVocab = Object.keys(capitalizedDictionary).sort().join(', ');
+        const vocab = Object.keys(dictionary).sort().join(', ');
+
+        console.log(capitalizedVocab + '\n\n' + vocab);
+    }
+
+    static containsNumbers (word) {
+        return /\d/.test(word);
+    }
+
+    static alphanumericOnly (string) {
+        return string.replace(/[^\w\s]|_/g, '');
+    }
+}
+
+BibleWords.FILES = {
+    bible: './bible-ukjv-preprocessed.txt',
+    dante: './inferno.txt',
+    wollstonecraft: './wollstonecraft-rights-of-women.txt',
+    wells: './history-of-the-world-wells.txt'
+};
+
+BibleWords.run(BibleWords.FILES.dante);
+
+// Corpus ideas
+// Frankenstien
+// Children's encyclopedia
+/* or wikipedia stuff, simple english, wikibooks
+newspaper archives / collections
+history books? overviews
+
+*/
